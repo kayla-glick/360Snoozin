012200cd672f40b0d0a2dba1a064f915631ba6d5
Kyle Glick <glickk@musc.edu>
2017-03-16 15:55:11 -0400

    adding base methods for alarm

--- 360SnoozinAlarmClock/src/com/csci360/alarmclock/Alarm.java
+++ 360SnoozinAlarmClock/src/com/csci360/alarmclock/Alarm.java
@@ -4,7 +4,47 @@
  * and open the template in the editor.
  */
 package com.csci360.alarmclock;
+import java.time.Instant;
 
 public class Alarm {
     
+    public Alarm() {
+
+    }
+    
+    public Instant getTime() {
+        return null;
+    }
+    
+    public void setTime(Instant time) {
+        
+    }
+    
+    public Instant getSnoozeTime() {
+        return null;
+    }
+    
+    public void setSnoozeTime(Instant snoozeTime) {
+        
+    }
+    
+    public boolean getIsActive() {
+        return false;
+    }
+    
+    public void setIsActive(boolean isActive) {
+        
+    }
+    
+    public boolean getIsSounding() {
+        return false;
+    }
+    
+    public void setIsSounding(boolean isSounding) {
+        
+    }
+    
+    public void snooze() {
+        
+    }
 }
