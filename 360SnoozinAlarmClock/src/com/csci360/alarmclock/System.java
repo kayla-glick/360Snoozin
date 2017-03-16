86075c8d0a21e7346873756f2de187bfb0a4a541
Kyle Glick <glickk@musc.edu>
2017-03-16 15:11:18 -0400

    adding only system methods

--- 360SnoozinAlarmClock/src/com/csci360/alarmclock/System.java
+++ 360SnoozinAlarmClock/src/com/csci360/alarmclock/System.java
@@ -7,4 +7,62 @@ package com.csci360.alarmclock;
 
 public class System {
     
+    // Return and param types may need to be changed
+    public void setClockTime(String time) {
+        
+    }
+    
+    // Return and param types may need to be changed
+    public void setAlarmTime(String time) {
+        
+    }
+    
+    // Return type may need to be changed
+    public void playRadio() {
+        
+    }
+    
+    // Return type may need to be changed
+    public void turnOffRadio() {
+        
+    }
+    
+    // Return type may need to be changed
+    public void toggleTimeFormat() {
+        
+    }
+    
+    // Return type may need to be changed
+    public void enableAlarm(int n) {
+        
+    }
+    
+    // Return type may need to be changed
+    public void disableAlarm(int n) {
+        
+    }
+    
+    // Return type may need to be changed
+    public void soundAlarms() {
+        
+    }
+    
+    public void snoozeAlarms() {
+        
+    }
+    
+    // Return type may need to be changed
+    public void toggleAMFM() {
+        
+    }
+    
+    // Return and param types may need to be changed
+    public void changeStation(String direction) {
+        
+    }
+    
+    // Return and param types may need to be changed
+    public void adjustRadioVolume(String direction) {
+        
+    }
 }
