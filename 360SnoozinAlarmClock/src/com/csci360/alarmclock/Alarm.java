01e2e00896c89c16823f7766428d533b4f10dcf3
Kyle Glick <glickk@musc.edu>
2017-03-18 18:40:49 -0400

    first iteration

--- 360SnoozinAlarmClock/src/com/csci360/alarmclock/Alarm.java
+++ 360SnoozinAlarmClock/src/com/csci360/alarmclock/Alarm.java
@@ -4,7 +4,66 @@
  * and open the template in the editor.
  */
 package com.csci360.alarmclock;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 
 public class Alarm {
+    public final static int SNOOZE_INTERVAL = 10;
+    public final static ChronoUnit SNOOZE_UNIT = ChronoUnit.MINUTES;
+
+    private Instant time;
+    private Instant snoozeTime;
+    private boolean isActive;
+    private boolean isSounding;
     
+    public Alarm() {
+        this.time = null;
+        this.snoozeTime = null;
+        this.isActive = false;
+        this.isSounding = false;
+    }
+    
+    public Instant getTime() {
+        return time;
+    }
+    
+    public void setTime(Instant time) {
+        this.time = time;
+        this.setSnoozeTime(time);
+    }
+    
+    public Instant getSnoozeTime() {
+        return snoozeTime;
+    }
+    
+    private void setSnoozeTime(Instant snoozeTime) {
+        this.snoozeTime = snoozeTime;
+    }
+    
+    public boolean getIsActive() {
+        return isActive;
+    }
+    
+    public void setIsActive(boolean isActive) {
+        this.isActive = isActive;
+        
+        if ( isActive ) {
+            this.setSnoozeTime(this.time);
+        }
+    }
+    
+    public boolean getIsSounding() {
+        return isSounding;
+    }
+    
+    public void setIsSounding(boolean isSounding) {
+        this.isSounding = isSounding;
+    }
+    
+    public void snooze() {
+        Instant newSnoozeTime = this.snoozeTime.plus(SNOOZE_INTERVAL, SNOOZE_UNIT);
+        
+        this.setSnoozeTime(newSnoozeTime);
+        this.isSounding = false;
+    }
 }
--- 360SnoozinAlarmClock/test/com/csci360/alarmclock/AlarmTest.java
+++ 360SnoozinAlarmClock/test/com/csci360/alarmclock/AlarmTest.java
@@ -5,6 +5,7 @@
  */
 package com.csci360.alarmclock;
 
+import java.time.Instant;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -17,12 +18,11 @@ import static org.junit.Assert.*;
  * @author kyleglick
  */
 public class AlarmTest {
-    
-    public AlarmTest() {
-    }
+    protected Alarm alarm;
     
     @BeforeClass
     public static void setUpClass() {
+        java.lang.System.out.println("Begin AlarmTest");
     }
     
     @AfterClass
@@ -31,6 +31,10 @@ public class AlarmTest {
     
     @Before
     public void setUp() {
+        alarm = new Alarm();
+        Instant time = Instant.parse("2000-01-01T12:30:00Z");
+        
+        alarm.setTime(time);
     }
     
     @After
@@ -38,9 +42,33 @@ public class AlarmTest {
     }
 
     @Test
-    public void testSomeMethod() {
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testSetIsActive() {
+        alarm.snooze();
+        
+        Instant snoozeTime = alarm.getTime().plus(Alarm.SNOOZE_INTERVAL, Alarm.SNOOZE_UNIT);
+                
+        java.lang.System.out.println("---Testing <setIsActive>");
+        java.lang.System.out.println("------If isActive == false, should not reset snoozeTime");
+        alarm.setIsActive(false);
+        assert(alarm.getSnoozeTime().equals(snoozeTime));
+        
+        java.lang.System.out.println("------If isActive == true, should reset snoozeTime");
+        alarm.setIsActive(true);
+        assert(alarm.getSnoozeTime().equals(alarm.getTime()));
     }
     
+    @Test
+    public void testSnooze() {
+        alarm.setIsSounding(true);
+        alarm.snooze();
+        
+        Instant snoozeTime = alarm.getTime().plus(Alarm.SNOOZE_INTERVAL, Alarm.SNOOZE_UNIT);
+        
+        java.lang.System.out.println("---Testing <snooze>");
+        java.lang.System.out.println("------Should increment snoozeTime");      
+        assertTrue(alarm.getSnoozeTime().equals(snoozeTime));
+        
+        java.lang.System.out.println("------Should set isActive = false");
+        assertFalse(alarm.getIsActive());
+    }
 }
